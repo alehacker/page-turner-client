e7a435f27d7e5de7ebcc481283c5a8656d461530
Ale Kennedy <alepertuz@gmail.com>
2023-03-07 21:15:36 -0500

    styling done on search page

--- src/pages/BookClubList.js
+++ src/pages/BookClubList.js
@@ -20,21 +20,21 @@ const BookClubList = () => {
  
 
    return (
-      <div className="py-8 mt-8 text-green-700 text-opacity-75 bg-gray-100">
+      <div className="py-8 mt-8 text-green-700 text-opacity-75 bg-white">
          <h1 className="mb-8 text-4xl font-bold text-center">Book Clubs</h1>
          <div className="max-w-4xl mx-auto">
          {
             bookClubs ? 
-            <>
+            <div className="grid grid-cols-3 gap-4 mb-4">
                { 
                   bookClubs.map((bookClub) => (
-               <div className="p-6 mb-4 bg-white rounded-lg shadow" key={bookClub._id}>
+               <div className="p-4 border border-gray-300 rounded-md" key={bookClub._id}>
                   <h2 className="mb-2 text-2xl font-bold">{bookClub.name}</h2>
                   <p className="text-green-700 text-opacity-75">{bookClub.description}</p> 
                </div>
                ))
                }
-            </>          
+            </div>          
 
             : 
 
@@ -47,3 +47,5 @@ const BookClubList = () => {
 }
 
 export default BookClubList
+
+
--- src/pages/SearchBook.js
+++ src/pages/SearchBook.js
@@ -28,29 +28,130 @@ const SearchBook = () => {
 //   }, [books]);
 
   return (
-    <div>
-      <form  className="flex flex-col items-center justify-between mt-5 md:ml-10 md:mt-0" onSubmit={handleSubmit}>
-      <label className="text-green-700 text-opacity-75">Search</label>
-        <input type="text" value={searchTerm} onChange={handleSearchTermChange} className="px-4 py-2 border border-green-700 border-opacity-50" placeholder='search book' />
+    <div className="flex flex-col items-center justify-between mt-5 md:ml-10 md:mt-0" >
+      <form  className="flex flex-col items-center justify-between mt-8 md:ml-10 md:mt-0" onSubmit={handleSubmit}>
+      <label className="mt-8 text-lg font-bold text-green-700 text-opacity-75">Search</label>
+        <input type="text" value={searchTerm} onChange={handleSearchTermChange} className="px-4 py-2 border border-green-700 border-opacity-50" placeholder='Search Book...' />
         <button className="w-1/2 px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded hover:bg-green-500" type="submit" >Search</button>
       </form>
       {console.log("BOOKS", books)}
 
-      <ul>
+      <ul className="grid grid-cols-4 gap-4 mb-4">
         {books ? books.map((book) => {
    
          
          console.log('book==>', book)
          return(
-          <li key={book.id}>
-          <Link to={`/book-details/${book.id}`} >
-            <h3>{book.volumeInfo.title}</h3>        
+          <li key={book.id} className="p-4 border border-gray-300 rounded-md">
+          <Link to={`/book-details/${book.id}`} className="text-green-700 hover:text-green-500 focus:text-green-500" >
+            <h3 className="text-lg font-bold">{book.volumeInfo.title}</h3>        
           </Link>
+          {book.volumeInfo.imageLinks?.thumbnail ? 
+            <img src={book.volumeInfo.imageLinks?.thumbnail} alt="Book cover" className="w-48 h-auto mb-2"/>
+      
+            : <p className="mt-2">Image not available</p>
+         }
           </li>
          )}): <p> </p>}
       </ul>
     </div>
   );
+// return (
+//    <div className="flex flex-col items-center justify-between mt-5 md:flex-row md:ml-10 md:mt-0">
+//       <div className="max-w-lg mb-5 md:max-w-xl md:mb-0">
+//                <img
+//                   className="w-full h-auto"
+//                   src="/images/peoplereading.jpg"
+//                   alt="Book Clubs"
+//                />
+//       </div>
+//      <form className="flex flex-col items-center justify-between w-full md:w-1/3" onSubmit={handleSubmit}>
+//        <label className="mb-2 text-green-700 text-opacity-75">Search</label>
+//        <div className="flex items-center w-full bg-white border border-green-700 border-opacity-50 ">
+//          <input
+//            type="text"
+//            value={searchTerm}
+//            onChange={handleSearchTermChange}
+//            className="w-full px-4 py-2 rounded-full focus:outline-none"
+//          // className="px-4 py-2 border border-green-700 border-opacity-50"
+//            placeholder="Search Book..."
+//          />
+//          <button
+//          //   className="px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded-full hover:bg-green-500"
+//          className="w-1/2 px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded hover:bg-green-500"
+//            type="submit"
+//          >
+//            Search
+//          </button>
+//        </div>
+//      </form>
+//      <div className="ml-0 md:w-2/3 md:ml-10">
+//        {console.log("BOOKS", books)}
+//        <ul className="grid grid-cols-1 gap-4 mb-4 md:grid-cols-4">
+//          {books
+//            ? books.map((book) => {
+//                console.log("book==>", book);
+//                return (
+//                  <li key={book.id} className="p-4 border border-gray-300 rounded-md">
+//                    <Link
+//                      to={`/book-details/${book.id}`}
+//                      className="text-green-700 hover:text-green-500 focus:text-green-500"
+//                    >
+//                      <h3 className="text-lg font-bold">{book.volumeInfo.title}</h3>
+//                    </Link>
+//                    {book.volumeInfo.imageLinks?.thumbnail ? (
+//                      <img
+//                        src={book.volumeInfo.imageLinks?.thumbnail}
+//                        alt="Book cover"
+//                        className="w-48 h-auto mb-2"
+//                      />
+//                    ) : (
+//                      <p className="mt-2">Image not available</p>
+//                    )}
+//                  </li>
+//                );
+//              })
+//            : <p></p>}
+//        </ul>
+//      </div>
+//    </div>
+//  );
+ 
 }
 
 export default SearchBook;
+
+
+{/* <div className="flex flex-col items-center justify-center w-full mt-10 md:flex-row">
+
+<div className="max-w-lg mb-5 md:max-w-xl md:mb-0">
+   <img
+      className="w-full h-auto"
+      src="/images/peoplereading.jpg"
+      alt="Book Clubs"
+   />
+</div>
+<div className="flex flex-col items-center justify-between mt-5 space-y-4 md:ml-10 md:mt-0">
+   <h1 className="mb-2 text-xl font-bold text-green-700 text-opacity-75"> Sign Up </h1>
+   
+      <form className="flex flex-col justify-between mt-5 md:ml-10 md:mt-0" onSubmit={handleSubmit} >
+
+         <label className="text-green-700 text-opacity-75" >First Name</label>
+         <input type='text' name="firstName" value={newUser.firstName} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+         
+         <label className="text-green-700 text-opacity-75">Last Name</label>
+         <input type='text' name="lastName" value={newUser.lastName} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+
+         <label className="text-green-700 text-opacity-75">Email</label>
+         <input type='email' name="email" value={newUser.email} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+
+         <label className="text-green-700 text-opacity-75">Password</label>
+         <input type='password' name="password" value={newUser.password} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+         
+         <label className="text-green-700 text-opacity-75">Profile Image</label>
+         <input type='file' name="profileImage" value={newUser.profileImage} onChange={handleChange} accept=".jpg, .jpeg, .png, .pdf" className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+
+         <button className="px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded hover:bg-green-500" type="submit"  >Sign Up</button>
+      </form>
+</div>
+</div> */}
