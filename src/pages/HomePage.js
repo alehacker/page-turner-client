47ca7e5387b30a6ba1f9a72bb784376817dd070b
Ale Kennedy <alepertuz@gmail.com>
2023-03-08 20:04:27 -0500

    changes to homepage and navbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,32 +6,36 @@ import BookClubList from "./BookClubList";
 const HomePage = () => {
    return (
       <div>
-         <div className="flex flex-col md:flex-row justify-center items-center mt-10">
-            <div className="max-w-lg md:max-w-xl mb-5 md:mb-0">
+         <div className="flex flex-col items-center justify-center mt-10 md:flex-row">
+            <div className="max-w-lg mb-5 md:max-w-xl md:mb-0">
                <img
                   className="w-full h-auto"
                   src="/images/book-clubs-940x529.jpg"
                   alt="Book Clubs"
                />
             </div>
-            <div className="md:ml-10 mt-5 md:mt-0 flex flex-col justify-between">
-               <h4 className="text-green-700 text-opacity-75 text-xl font-bold mb-2">
+            <div className="flex flex-col justify-between mt-5 md:ml-10 md:mt-0">
+               <h4 className="mb-2 text-xl font-bold text-green-700 text-opacity-75">
                   Wherever you are, find your people
                </h4>
-               <p className="text-green-700 text-opacity-75 text-base mb-5">
+               <p className="mb-5 text-base text-green-700 text-opacity-75">
                   Find, create and organize your bookclubs here!
                </p>
                <div>
-                  <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-2 my-4">
+               <Link to={'/bookclubs'}>
+                  <button className="px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 rounded hover:bg-green-500">
                         Find Book Clubs
                   </button>
-                  <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+               </Link>
+               <Link to={'/books'}>
+                  <button className="px-4 py-2 font-bold text-white bg-green-700 rounded hover:bg-green-500">
                         Search Books
                   </button>
+               </Link>
                </div>
             </div>
          </div>
-         <div className="flex flex-col md:flex-row justify-center items-center mt-10">
+         <div className="flex flex-col items-center justify-center mt-10 md:flex-row">
             <div className="mt-5 md:mt-0">
                <BookClubList />
             </div>
