3354345c76ec77332cf90b141e8693e4cb7e00b2
Ale Kennedy <alepertuz@gmail.com>
2023-03-21 14:33:01 -0500

    fixing edit profile

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -6,7 +6,7 @@ import { post } from "../services/authService"
 import axios from "axios"
 
 function EditProfilePage() {
-   const {user, setUser, books, setBooks, message } = useContext(LoadingContext)
+   const {user, setUser, message } = useContext(LoadingContext)
    const { authenticateUser } = useContext(AuthContext) 
    
    const navigate = useNavigate()
@@ -17,12 +17,13 @@ function EditProfilePage() {
       setUser((recent) => ({...recent, [e.target.name]: e.target.value}))
   }
 
-   const handleFile = (e) => {
-      setFile(e.target.files[0])
-  }
+  const handleFile = (e) => {
+   setFile(e.target.files[0])
+}
    
    const handleSubmit = (e) =>{
       e.preventDefault()
+      
       handleUpload()
       .then((response) => {
          post(`/users/profile-edit/${user._id}`,{...user, profileImage: response})
@@ -43,17 +44,17 @@ function EditProfilePage() {
       })
    }
 
-   const handleUpload = async() => {
+   const handleUpload =  async() => {
       try {
-          const uploadData = new FormData()
-          uploadData.append('profileImage', file)
-          const response = await post('/auth/upload-photo', uploadData)
-          console.log(response)
-          return(response.data.url)
-      } catch (error) {
-          console.log(error)
-      }
-  }
+         const uploadData = new FormData()
+         uploadData.append('profileImage', file)
+         const response = await post('/auth/upload-photo', uploadData)
+         console.log('Here is the response ===>',response)
+         return(response.data.url)
+     } catch (error) {
+         console.log(error)
+     }
+   }
 
  
    return (
@@ -67,25 +68,31 @@ function EditProfilePage() {
             </div>
             <div className="flex flex-col items-center justify-between mt-5 space-y-4 md:ml-10 md:mt-0">
                <h1 className="mb-2 text-xl font-bold text-green-700 text-opacity-75">Edit Profile </h1>
-               <form className="flex flex-col justify-between mt-5 md:ml-10 md:mt-0" onSubmit={handleSubmit}>
-                  
-                  <label className="text-green-700 text-opacity-75" >First Name</label>
-                  <input type='text' name="firstName" value={user.firstName} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
-                  
-                  <label className="text-green-700 text-opacity-75">Last Name</label>
-                  <input type='text' name="lastName" value={user.lastName} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
-                  
-                  <label className="text-green-700 text-opacity-75">Email</label>
-                  <input type='email' name="email" value={user.email} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+               {user &&
+                  <>
+                     <form className="flex flex-col justify-between mt-5 md:ml-10 md:mt-0" onSubmit={handleSubmit}>
+                     
+                        
+                        <label className="text-green-700 text-opacity-75" >First Name</label>
+                        <input type='text' name="firstName" value={user.firstName} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+                        
+                        <label className="text-green-700 text-opacity-75">Last Name</label>
+                        <input type='text' name="lastName" value={user.lastName} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+                        
+                        <label className="text-green-700 text-opacity-75">Email</label>
+                        <input type='email' name="email" value={user.email} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
 
-                  <label className="text-green-700 text-opacity-75">Profile Image</label>
-                     <input type='file' name="profileImage" 
-                     // value={user.profileImage} 
-                     onChange={handleFile}  className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+                        <label className="text-green-700 text-opacity-75">Profile Image</label>
+                           <input type='file' name="profileImage" 
+                           // value={user.profileImage} 
+                           onChange={handleFile}  className="px-4 py-2 border border-green-700 border-opacity-50"></input>
 
-                  <button className="px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded hover:bg-green-500" type="submit">Edit Profile</button>
-                 
-               </form>
+                        <button className="px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded hover:bg-green-500" type="submit">Edit Profile</button>
+                     
+                     </form>
+                     
+                  </>
+               }
             </div>
             {message && <p className="text-white bg-green-700 bg-opacity-75 error">{message}</p>}
       </div>
