ee1803e955c6a0b9afef935c3de9c863b649a68e
Ale Kennedy <alepertuz@gmail.com>
2023-03-21 15:37:19 -0500

    debugging Edit BookClub

--- src/pages/EditBookClub.js
+++ src/pages/EditBookClub.js
@@ -24,11 +24,12 @@ function EditBookClubPage() {
   
   const handleSubmit = (e) =>{
      e.preventDefault()
+     
      handleUpload()
      .then((response) =>{
         post(`/bookclubs/edit-bookclub/${bookclubId}/${userId}`,  {...bookClub, clubImg: response })
         .then((results)=>{
-           console.log('results--->', results)
+           console.log('Edited Book Club--->', results.data)
            setBookClub(results.data)
            navigate(`/bookclub-details/${bookclubId}`)
          })
@@ -44,7 +45,7 @@ function EditBookClubPage() {
         const uploadData = new FormData()
         uploadData.append('profileImage', file)
         const response = await post('/auth/upload-photo', uploadData)
-        console.log(response)
+        console.log('Here is the new images URL ==>>',response)
         return(response.data.url)
     } catch (error) {
         console.log(error)
@@ -84,9 +85,7 @@ function EditBookClubPage() {
                   <input type='text' name="meetingLink" value={bookClub.meetingLink} onChange={handleChange} className="px-4 py-2 border border-green-700 border-opacity-50"></input>
 
                   <label className="text-green-700 text-opacity-75">Book Club Image</label>
-                  <input type='file' name="clubImg" 
-                  // value={bookClub.clubImg} 
-                  onChange={handleFile}  className="px-4 py-2 border border-green-700 border-opacity-50"></input>
+                  <input type='file' name="clubImg" onChange={handleFile}  className="px-4 py-2 border border-green-700 border-opacity-50"></input>
 
                   <button className="px-4 py-2 my-4 mr-2 font-bold text-white bg-green-700 bg-opacity-75 rounded hover:bg-green-500" type="submit">Edit Book Club</button>
                  
@@ -96,6 +95,7 @@ function EditBookClubPage() {
                
                }
             </div>
+            
             {message && <p className="text-white bg-green-700 bg-opacity-75 error">{message}</p>}
       </div>
     )
@@ -103,29 +103,3 @@ function EditBookClubPage() {
 
 export default EditBookClubPage
 
-// The code below handles the change on images with Cloudinary
-// const handleFileUpload = (e) => {
-
-//    console.log("Uploading photo...")
-
-//      const uploadData = new FormData()
-//      uploadData.append('profileImage', e.target.files[0])
-//      console.log("Upload data" , uploadData, e.target.files)
-//      post('/users/new-profile-photo', uploadData)
-//        .then((result) => {
-//          setProfileImage(result.data.profileImage)
-//          console.log("This is photo", result.data)
-//        })
-//        .catch((err) => {
-//          console.log("Upload error", err)
-//        })
-//  }
-
-
-//  <label>
-//             Profile Picture:
-//             <input type="file" name="profileImage" 
-//             onChange={(e) => handleFileUpload(e)}
-//             // onChange={(e) => setProfileImage(e.target.value)}
-//              />
-//           </label>
